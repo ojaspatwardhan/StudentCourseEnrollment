e4d82e7189ad76b57c80778cd4ae73022b5a4fa4
Ojas Patwardhan <ojaspatwardhan@Ojass-MacBook-Pro.local>
2018-07-31 01:39:47 +0530

    Heroku deploy changes

--- server.js
+++ server.js
@@ -1,48 +1,16 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var session = require('express-session');
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/webdev-summer-2018');
+//Install express server
+const express = require('express');
+const path = require('path');
 
-app.use(session({
- resave: false,
- saveUninitialized: true,
- secret: 'any string'
-}));
+const app = express();
 
-app.use(bodyParser.json());
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/hello-world-angular'));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/*', function(req,res) {
 
-app.get('/message/:theMessage', (req, res) => {
-  var theMessage = req.params['theMessage'];
-  res.send(theMessage);
-})
+res.sendFile(path.join(__dirname+'/dist/hello-world-angular/index.html'));
+});
 
-app.get('/api/session/set/:name/:value',
-setSession);
-app.get('/api/session/get/:name',
-getSession);
-// app.get('/api/session/get',
-// getSessionAll);
-// app.get('/api/session/reset',
-// resetSession);
-
-function setSession(req, res) {
- var name = req.params['name'];
- var value = req.params['value'];
- req.session[name] = value;
- res.send(req.session);
-}
-
-function getSession(req, res) {
- var name = req.params['name'];
- var value = req.session[name];
- res.send(value);
-}
-
-var userService = require("./services/user.service.server");
-userService(app);
-
-app.listen(4000, () => console.log('Example app listening on port 4000!'))
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
